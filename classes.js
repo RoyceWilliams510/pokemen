0b1f2d2cacf0e63bd66946c826102329df2af742
brysuu <roycejameswilliams@students.berkeley.net>
2019-06-07 13:09:35 -0700

    revised constructor for the pokemon class

--- classes.js
+++ classes.js
@@ -23,19 +23,27 @@ class Move{
 }
 
 class Trainer{
-    constructor(name){
-        this.name = name;
+    constructor(){
         this.teamCount =0;
         this.pokemons = [];
         this.currentPokemon =[];
     }
     addPokemon(pokemonId){
-        for(var i= 0;i<this.currentPokemon.length; i++){
-            if(this.currentPokemon[i].pokemonId){
-
+        if(this.pokemons.length<=3) {
+            for (var i = 0; i < allPokemon.length; i++) {
+                if (pokemonId == allPokemon[i].id) {
+                    this.pokemons.push(allPokemon[i]);
+                    alert(this.name+" has added "+allPokemon[i].name+" to their team.");
+                }
             }
+        }else{
+            alert("You already have a full team!");
         }
     }
+    setName(name){
+        this.name= name;
+    }
+
     switchPokemon(pokemonId){
     }
 }
@@ -159,11 +167,38 @@ var machamp=new Pokemon("Machamp", fighting, 290, 103, 157, 238,7);
 var gengar=new Pokemon("Gengar", ghost, 230, 202, 139, 238, 8);
 var exegggutor=new Pokemon("Exeggutor", grass, 300, 103, 157, 229,9);
 var snorlax=new Pokemon("Snorlax", normal, 430, 58, 202, 202, 10);
-var hitmonlee=new Pokemon("Hitmonlee", fighting, 210, 161, 202, 220,11);
+var hitmonlee=new Pokemon("Hitmonlee", fighting, 210, 161, 202, 220, 11);
+var dragonite=new Pokemon ("Dragonite", dragon, 148, 175, 245, 12);
+var dodrio=new Pokemon ("Dodrio", flying, 230, 202, 130, 202, 13);
+var muk=new Pokemon ("Muk", poison, 320, 94, 184, 193, 14);
+var lapras=new Pokemon ("Lapras", water, 370, 112, 175, 157, 15);
+var rhydon=new Pokemon ("Rhydon", rock, 320, 76, 220, 238, 16);
+var golem=new Pokemon ("Golem", rock, 270, 85, 238, 220, 17);
+var jynx=new Pokemon ("Jynx", ice, 240, 175, 175, 211, 18);
+var jolteon=new Pokemon ("Jolteon", electricity, 240, 238, 175, 202, 19);
+var articuno=new Pokemon("Articuno", ice, 290, 157, 229, 175, 20);
+var zapdos=new Pokemon("Zapdos", electricity, 290, 184, 166, 229, 21);
+var moltres=new Pokemon ("Moltres", fire, 290, 166, 166, 229, 22);
+var suicine=new Pokemon("Suicune", water, 310, 157, 211, 166, 23);
+var entei=new Pokemon("Entei", fire, 340, 184, 157, 211, 25);
+var raikou=new Pokemon("Raikou", electricity, 290, 211, 184, 211, 26);
+var mewtwo=new Pokemon("Mewtwo", psychic, 322, 238, 166, 281, 27);
+var lugia=new Pokemon("Lugia", flying, 322, 202, 281, 166, 28);
+var hooh=new Pokemon("Ho-oh",  flying, 322, 166, 281, 238, 29);
+var celebi=new Pokemon("Celebi", grass, 310, 184, 184, 184, 30);
+var regirock=new Pokemon("Regirock", rock, 270, 94, 364, 184, 31);
+var latias=new Pokemon("Latias", dragon, 270, 202, 238, 202, 32);
+var kyogre=new Pokemon("Kyogre", water, 310, 166, 256, 274, 33);
+var groudon=new Pokemon("Groudon", ground, 310, 166, 256, 274, 34);
+var rayquaza=new Pokemon("Rayquaza", dragon, 320, 175, 166, 274, 35);
+var jirachi= new Pokemon("Jirachi", psychic, 310, 184, 184, 184, 36);
+var deoxys=new Pokemon("Deoxys", psychic, 210, 274, 94, 274, 37);
 
-
-var allPokemon =[venosaur,charizard,blastoise,raichu,arcanine,alakazam,machamp,gengar,exegggutor,snorlax,hitmonlee];
-
+var allPokemon =[venosaur,charizard,blastoise,raichu,arcanine,alakazam,machamp,gengar,exegggutor,snorlax,hitmonlee,dragonite
+    ,dodrio,muk,lapras,rhydon,golem,jynx,jolteon,articuno,zapdos,moltres,suicine,entei,raikou,mewtwo,lugia,hooh,celebi,regirock
+    ,latias,kyogre,groudon,rayquaza,jirachi,deoxys];
+var trainer1 = new Trainer();
+var trainer2 = new Trainer();
 
 
 
--- pokemon.js
+++ pokemon.js
@@ -3,17 +3,70 @@ $(document).ready(function() {
     $("#go").click(function(){
         var trainer1Name =$("#trainer1Name").val();
         var trainer2Name = $("#trainer2Name").val();
-        var trainer1 = new Trainer(trainer1Name);
-        var trainer2 = new Trainer(trainer2Name);
+        trainer1.setName(trainer1Name);
+        trainer2.setName(trainer2Name);
         console.log(trainer1);
         console.log(trainer2);
         $("#nameTaker").hide();
         alert("Hello "+ trainer1.name+" and "+trainer2.name+" welcome to our game. Your next step is to go and select the available pokemon below to battle each other with.");
         $("#intro").hide();
+        constructMenus();
     });
 });
+//newPokemon(name, type, health, speed, defense, attack, id)
 function constructMenus(){
+    var table2 = "<table id ='pokemonMenu2' class='menuCharts'><tr>"+trainer1.name+"'s Menu</tr>" +
+            "<tr>" +
+        "    <th>Name</th>" +
+        "    <th>Type</th>" +
+        "    <th>Health</th>" +
+        "    <th>Speed</th>"+
+        "    <th>Defense</th>"+
+        "    <th>Attack</th>" +
+               "  </tr>";
+    var table1 = "<table id ='pokemonMenu1'><tr>"+trainer2.name+"'s Menu</tr>" +
+        "<tr>" +
+        "    <th>Name</th>" +
+        "    <th>Type</th>" +
+        "    <th>Health</th>" +
+        "    <th>Speed</th>" +
+        "    <th>Defense</th>" +
+        "    <th>Attack</th>" +
+        "  </tr>";
+    for(var i =0; i<allPokemon.length; i++){
+        table1+="<tr><td>"+allPokemon[i].name+"</td>" +
+            "<td>"+allPokemon[i].type.name+"</td>"+
+            "<td>"+allPokemon[i].health+"</td>"+
+            "<td>"+allPokemon[i].speed+"</td>"+
+            "<td>"+allPokemon[i].defense+"</td>"+
+            "<td>"+allPokemon[i].attackPow+"</td>" +
+            "<td><button id ='add"+allPokemon[i].id+"'value ='"+allPokemon[i].id+"' onclick='addPokemon2("+allPokemon[i].id+")'>Add to team</button></td></tr>"
+    }
 
+    for(var k =0; k<allPokemon.length; k++){
+        table2+="<tr><td>"+allPokemon[k].name+"</td>" +
+            "<td>"+allPokemon[k].type.name+"</td>"+
+            "<td>"+allPokemon[k].health+"</td>"+
+            "<td>"+allPokemon[k].speed+"</td>"+
+            "<td>"+allPokemon[k].defense+"</td>"+
+            "<td>"+allPokemon[k].attackPow+"</td>" +
+            "<td><button id ='add"+allPokemon[k].id+"' value ='"+allPokemon[k].id+"' onclick='addPokemon1("+allPokemon[k].id+")'>Add to team</button></td></tr>"
+    }
+    $("#trainer1Menu").html(table1);
+    $("#trainer2Menu").html(table2);
 }
 
-
+function addPokemon1(id){
+    trainer1.addPokemon(id);
+    console.log(trainer1.pokemons);
+}
+function addPokemon2(id){
+    trainer2.addPokemon(id);
+    for(var i= 0;i<allPokemon.length; i++){
+        if(id ==allPokemon[i].id){
+            var pokeName = allPokemon[i].name;
+        }
+    }
+    alert(trainer2.name+" has added "+pokeName+" to their team.");
+    console.log(trainer2.pokemons);
+}
